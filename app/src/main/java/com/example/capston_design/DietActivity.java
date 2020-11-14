449e5e0e8d5c064dd22bc649a7d6c84a78cb6282
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2020-11-15 02:03:13 +0900

    layout 진행

--- app/src/main/java/com/example/capston_design/DietActivity.java
+++ app/src/main/java/com/example/capston_design/DietActivity.java
@@ -1,14 +1,84 @@
 package com.example.capston_design;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class DietActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Context context =this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.diet_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.diet_drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.diet_nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+
+                int id = item.getItemId();
+                String title = item.getTitle().toString();
+
+                if (id==R.id.tab_info){
+                    Intent intent = new Intent(DietActivity.this, InformationActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else if (id==R.id.tab_eat){
+                    Intent intent = new Intent(DietActivity.this, DietActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                else if (id==R.id.tab_yoga){
+                    Intent intent = new Intent(DietActivity.this, YogaActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                else if (id==R.id.tab_hos){
+                    Intent intent = new Intent(DietActivity.this, HospitalActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
